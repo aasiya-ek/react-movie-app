67b3e9263605682cd8ad689cfb1cc0c65fbce153
Aasiya Ekram <ekramaasiya98@gmail.com>
2026-05-06 17:18:13 +0530

    Files Modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import "./App.css";
 import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <>
-    <main>
+    <div>
+      <Navbar />
+    <main className="main-content">
       <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/favourites" element={<Favourites/>} />
       </Routes>
       </main>
-    </>
+      </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">Movie App</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/favorites" className="nav-link">
+          Favourites
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
